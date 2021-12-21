7e65a14f542572dbc206b36e42240fb41be9b076
William Davidson <william11231999@gmail.com>
2021-12-21 14:27:36 -0500

    Redid how the objects are drawn, added clickability to the window, finished the 'exit' functionality and started on the 'help' menu

--- Main_Menu.py
+++ Main_Menu.py
@@ -2,57 +2,102 @@ from graphics import GraphWin, Rectangle, Text, Point, color_rgb, Image
 
 def mainMenu (window):
 
+    #Initialize variables
     win = window
+    menuObjects = []
+
     #Background
     background = Image(Point(50,50), "MMBackground.gif")
     background.draw(win)
-
 
     #Title Text
     titleBox = Rectangle(Point(10,95), Point(90, 75))
     titleBox.setFill(color_rgb(132,132,130))
     titleBox.setOutline("white")
-    titleBox.draw(win)
+    menuObjects.append(titleBox)
     titleText = Text(Point(50, 85), "CHECK THE BOARD")
     titleText.setSize(35)
-    titleText.draw(win)
+    menuObjects.append(titleText)
 
     #Start Button
     startBox = Rectangle(Point(30, 65), Point(70, 55))
     startBox.setFill(color_rgb(163,163,161))
     startBox.setOutline("white")
-    startBox.draw(win)
+    menuObjects.append(startBox)
     startText = Text(Point(50, 60), "Level Select")
     startText.setSize(20)
-    startText.draw(win)
+    menuObjects.append(startText)
 
     #Options Button
     optionsBox = Rectangle(Point(30, 50), Point(70, 40))
     optionsBox.setFill(color_rgb(163,163,161))
     optionsBox.setOutline("white")
-    optionsBox.draw(win)
+    menuObjects.append(optionsBox)
     optionsText = Text(Point(50, 45), "Options")
     optionsText.setSize(20)
-    optionsText.draw(win)
+    menuObjects.append(optionsText)
 
     #Help Button
     helpBox = Rectangle(Point(30, 35), Point(70, 25))
     helpBox.setFill(color_rgb(163,163,161))
     helpBox.setOutline("white")
-    helpBox.draw(win)
+    menuObjects.append(helpBox)
     helpText = Text(Point(50, 30), "Help")
     helpText.setSize(20)
-    helpText.draw(win)
+    menuObjects.append(helpText)
 
     #Exit Button
     exitBox = Rectangle(Point(30, 20), Point(70, 10))
     exitBox.setFill(color_rgb(163,163,161))
     exitBox.setOutline("white")
-    exitBox.draw(win)
+    menuObjects.append(exitBox)
     exitText = Text(Point(50, 15), "Exit")
     exitText.setSize(20)
-    exitText.draw(win)
-    
+    menuObjects.append(exitText)
+
+    #Draw in the Main Menu objects
+    for i in range(len(menuObjects)):
+        menuObjects[i].draw(win)
+
+    #Main Menu Functionality
+    clickText = Text(Point(50, 5), " ")
+    while True:
+        click = win.getMouse()
+        clickText.undraw()
+        clickText = Text(Point(50, 5), "X: " + str(format(click.getX(), ".2f")) + " " + "Y: " + str(format(click.getY(), ".2f")))
+        clickText.setSize(20)
+        clickText.setTextColor("red")
+        clickText.draw(win)
+        if click.getX() > 30 and click.getX() < 70 and click.getY() > 25 and click.getY() < 35:
+            helpScreen(win, menuObjects)
+            mainMenu(win)
+            break
+        if click.getX() > 30 and click.getX() < 70 and click.getY() > 10 and click.getY() < 20:
+            break
+
+
+def helpScreen(win, menuObjects):
+    #Initialize objects array
+    helpObjects = []
+
+    #Undraw the Main Menu Objects
+    for i in range(len(menuObjects)):
+        menuObjects[i].undraw()
+
+    #Create Help Menu
+    exitHelpBox = Rectangle(Point(30, 7), Point(70, 3))
+    exitHelpBox.setFill(color_rgb(163,163,161))
+    exitHelpBox.setOutline("white")
+    helpObjects.append(exitHelpBox)
+    exitHelpText = Text(Point(50, 5), "Click anywhere to go back to the Main Menu")
+    exitHelpText.setSize(10)
+    helpObjects.append(exitHelpText)
+
+    #Draw in the Help Menu objects
+    for i in range(len(helpObjects)):
+        helpObjects[i].draw(win)
+
+    #Click to exit the Help Menu
     win.getMouse()
 
 
@@ -62,6 +107,7 @@ def main():
     win = GraphWin("Check The Board", w , h)
     win.setCoords(0.0 ,0.0 ,100.0, 100.0)
     mainMenu(win)
+
 
 if __name__ == "__main__":
     main()
